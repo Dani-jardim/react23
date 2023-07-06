b08bcadb24754bfd00d6d67e4f8e8507441324e6
Daniela Jardim <Daniela.Jardim-ext@ab-Inbev.com>
2023-07-06 19:15:04 -0300

    condicional e map com react

--- 3_fundamentos_avancados/3_fundamentos_avancados/src/App.js
+++ 3_fundamentos_avancados/3_fundamentos_avancados/src/App.js
@@ -1,8 +1,19 @@
+import UserDetails from "./componentes/UserDetails";
 
 function App() {
+
+  const users = [
+    {id: 1, name:'Daniela', age: 1, job:'designer', habilitado:false},
+    {id: 2, name:'Luis', age: 30, job:'developer', habilitado:true},
+    {id: 3, name:'Juan', age: 28, job:'quality', habilitado:false}
+  ]
   return (
     <div className="App">
-      <h1>novos fundamentos</h1>
+      <div>
+      {users.map( (user) => (
+        <UserDetails key={user.id} name={user.name} age={user.age} profession={user.profession}/>
+      ))}
+      </div>
     </div>
   );
 }
